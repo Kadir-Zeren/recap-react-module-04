f21f71e7269860c58f251cf63cc1ac24a9226ad9
kadir-zeren <k4dzeren@gmail.com>
2024-03-26 17:37:31 +0300

    module ended

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,11 @@
 import Buton from "../buton/Buton";
-import "./Card.css";
+import CardStyle from "./Card.module.css";
 
 const Card = ({ lang, btn, img }) => {
   return (
     <div>
-      <h1>{lang}</h1>
-      <img src={img} alt="" />
+      <h1 className={CardStyle.title}>{lang}</h1>
+      <img className={CardStyle.images} src={img} alt="" />
       <Buton btn={btn} />
     </div>
   );

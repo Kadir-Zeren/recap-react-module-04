3ae2f6fca574a036d607074aba2effbcf941dfd7
kadir-zeren <k4dzeren@gmail.com>
2024-03-26 16:43:01 +0300

    card.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Card from "./components/card/Card";
 import data from "./data";
 function App() {
   console.log(data);
   return (
     <>
       <h1>APP</h1>
+      <Card />
     </>
   );
 }
